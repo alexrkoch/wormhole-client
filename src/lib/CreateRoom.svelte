c34f7882e6f7cbb1e782a6afdf39b533045f2b5e
Alex <alexrkoch@gmail.com>
2023-06-01 07:15:06 -0400

    create room component, home and room routes

---
+++ src/lib/CreateRoom.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  function createRoom(): void {
+    // call create-room API
+  }
+  export let buttonText: string;
+</script>
+
+<button on:click={createRoom}>
+  {buttonText}
+</button>
