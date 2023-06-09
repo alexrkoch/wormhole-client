d5b828e9ffff2e83b20ed2e4679df7d0a60a47c0
Alex <alexrkoch@gmail.com>
2023-06-09 17:16:12 -0400

    create JoinRoom component, minor changes to create room

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<script>
-	import CreateRoom from "$lib/CreateRoom.svelte";
+<script lang="ts">
+	import CreateRoom from '$lib/CreateRoom.svelte';
+	import JoinRoom from '$lib/JoinRoom.svelte';
 
-  let buttonText = "Create a New Room"
+	let buttonText: string = 'Create a New Room';
+	let roomCodePattern: RegExp = /^[A-Z]{6}$/;
 </script>
 
 <h1>Welcome to Wormhole!</h1>
 <p>Please create a new room or join an existing room below</p>
-<CreateRoom {buttonText}/>
+<CreateRoom {buttonText} />
+<JoinRoom {roomCodePattern} />
