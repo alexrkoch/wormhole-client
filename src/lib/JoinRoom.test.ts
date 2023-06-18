a20e8e7910dc533786326e8241fac2ec15e9daf9
Alex <alexrkoch@gmail.com>
2023-06-18 09:59:55 -0400

    configure testing and add tests

---
+++ src/lib/JoinRoom.test.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, fireEvent, screen, logDOM } from '@testing-library/svelte';
+import '@testing-library/jest-dom';
+import JoinRoom from './JoinRoom.svelte';
+
+describe('JoinRoom', () => {
+  
+	it('should accept any pattern if none passed in, enable button', async () => {
+		render(JoinRoom);
+    const inputField = screen.getByRole('textbox');
+    const button = screen.getByRole('button');
+    await fireEvent.input(inputField, {target: {value: "ABC"}});
+		expect(button).toBeEnabled();
+	});
+
+  it('should enable button only when pattern passed in is met', async () => {
+		render(JoinRoom, {props: {roomCodePattern: /^[A-Z]{6}$/}});
+		const inputField = screen.getByRole('textbox');
+    const button = screen.getByRole('button');
+    await fireEvent.input(inputField, {target: {value: "ABC"}});
+		expect(button).toBeDisabled();
+    await fireEvent.input(inputField, {target: {value: "ABCDEF"}});
+		expect(button).toBeEnabled();
+	});
+  
+});
--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,18 @@ test('room code page has expected h1', async ({ page }) => {
 	await page.goto('/room/WXYZ');
 	await expect(page.getByRole('heading', { name: 'Room: WXYZ' })).toBeVisible();
 });
+
+test('CreateRoom and JoinRoom render correctly, Join code entry validation', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('button', { name: 'Create a New Room' })).toBeVisible();
+  const joinButton = page.getByRole('button', { name: 'Join Room' });
+  const roomCodeInput = page.getByRole('textbox');
+  // join is disabled to start
+	await expect(joinButton).toBeDisabled();
+  // user inputs an invalid room code > join still disabled
+  await roomCodeInput.fill('ABC');
+	await expect(joinButton).toBeDisabled();
+  // user inputs an valid room code > join enabled
+  await roomCodeInput.fill('ABCDEF');
+	await expect(joinButton).not.toBeDisabled();
+});
