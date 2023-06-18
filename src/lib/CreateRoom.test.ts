14331d908fcf823a240bdd18e0dc77ce42d02c6d
Alex <alexrkoch@gmail.com>
2023-06-18 09:59:55 -0400

    configure testing and add tests

---
+++ src/lib/CreateRoom.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, fireEvent, screen, logDOM } from '@testing-library/svelte';
+import '@testing-library/jest-dom';
+import CreateRoom from './CreateRoom.svelte';
+
+describe('CreateRoom', () => {
+  
+	it('should render the create room button with defined label', async () => {
+		render(CreateRoom, {props: {buttonText: "Create Room"}});
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Create Room");
+	});
+
+});
