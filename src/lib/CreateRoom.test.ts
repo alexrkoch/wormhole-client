b14e52929826a33f702649724a00dd35bbdf957a
Alex <alexrkoch@gmail.com>
2023-06-19 19:31:21 -0400

    prettier config, run prettier, use testids

--- src/lib/CreateRoom.test.ts
+++ src/lib/CreateRoom.test.ts
@@ -1,15 +1,13 @@
-import { describe, it, expect, vi } from 'vitest';
-import { render, fireEvent, screen, logDOM } from '@testing-library/svelte';
-import '@testing-library/jest-dom';
-import CreateRoom from './CreateRoom.svelte';
+import { describe, it, expect, vi } from "vitest";
+import { render, fireEvent, screen, logDOM } from "@testing-library/svelte";
+import "@testing-library/jest-dom";
+import CreateRoom from "./CreateRoom.svelte";
 
-describe('CreateRoom', () => {
-  
-	it('should render the create room button with defined label', async () => {
-		render(CreateRoom, {props: {buttonText: "Create Room"}});
-    const button = screen.getByRole('button');
+describe("CreateRoom", () => {
+  it("should render the create room button with defined label", async () => {
+    render(CreateRoom, { props: { buttonText: "Create Room" } });
+    const button = screen.getByTestId("create-room-button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent("Create Room");
-	});
-
+  });
 });
--- tests/test.ts
+++ tests/test.ts
@@ -1,21 +1,21 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('room code page has expected h1', async ({ page }) => {
-	await page.goto('/room/WXYZ');
-	await expect(page.getByRole('heading', { name: 'Room: WXYZ' })).toBeVisible();
+test("room code page has expected h1", async ({ page }) => {
+  await page.goto("/room/WXYZ");
+  await expect(page.getByRole("heading", { name: "Room: WXYZ" })).toBeVisible();
 });
 
-test('CreateRoom and JoinRoom render correctly, Join code entry validation', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('button', { name: 'Create a New Room' })).toBeVisible();
-  const joinButton = page.getByRole('button', { name: 'Join Room' });
-  const roomCodeInput = page.getByRole('textbox');
+test("CreateRoom and JoinRoom render correctly, Join code entry validation", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByTestId("create-room-button")).toBeVisible();
+  const joinButton = page.getByTestId("join-button");
+  const roomCodeInput = page.getByTestId("room-input");
   // join is disabled to start
-	await expect(joinButton).toBeDisabled();
+  await expect(joinButton).toBeDisabled();
   // user inputs an invalid room code > join still disabled
-  await roomCodeInput.fill('ABC');
-	await expect(joinButton).toBeDisabled();
+  await roomCodeInput.fill("ABC");
+  await expect(joinButton).toBeDisabled();
   // user inputs an valid room code > join enabled
-  await roomCodeInput.fill('ABCDEF');
-	await expect(joinButton).not.toBeDisabled();
+  await roomCodeInput.fill("ABCDEF");
+  await expect(joinButton).not.toBeDisabled();
 });
