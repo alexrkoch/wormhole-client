8bcff22c267d0ff31b702e0007dd33853f9e566e
Alex <alexrkoch@gmail.com>
2023-06-19 20:20:44 -0400

    switch prettier to tabs and define tab size

--- src/lib/JoinRoom.svelte
+++ src/lib/JoinRoom.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  export let roomCodePattern: RegExp = /.*/; // default to match anything
-  $: input = "";
-  $: isValid = roomCodePattern.test(input);
+	export let roomCodePattern: RegExp = /.*/; // default to match anything
+	$: input = "";
+	$: isValid = roomCodePattern.test(input);
 
-  async function joinRoom(roomCode: string): Promise<void> {
-    // call join-room API
-  }
+	async function joinRoom(roomCode: string): Promise<void> {
+		// call join-room API
+	}
 </script>
 
 <form on:submit|preventDefault={() => joinRoom(input)}>
-  <input type="text" bind:value={input} data-testid="room-input" />
-  <button type="submit" disabled={!isValid} data-testid="join-button"> Join Room </button>
+	<input type="text" bind:value={input} data-testid="room-input" />
+	<button type="submit" disabled={!isValid} data-testid="join-button"> Join Room </button>
 </form>
